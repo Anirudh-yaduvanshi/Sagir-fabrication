2a6841b32bfd65a930a3c1f40135a94f690247db
Anirudh yadav <yadavanirudh97.25@gmail.com>
2025-04-15 01:10:41 +0530

    commit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-const Card = ({ image, name, phone, whatsapp, gmail, designation,id }) => {
+const Card = ({ image, name, phone, whatsapp, gmail, designation, id }) => {
+
   return (
     <div className=" card  w-4/5 margin md:h-64  grid md:grid-cols-2 md:gap-4">
       <div className="h-full  flex  justify-center  items-center  ">
@@ -30,7 +31,10 @@ const Card = ({ image, name, phone, whatsapp, gmail, designation,id }) => {
             {gmail}
           </a>
         </div>
-        <div className=" text-sm text-wrap hover:scale-110 hover:text-red-500">
+        <div
+        
+          className=" text-sm text-wrap hover:scale-110 hover:text-blue-500"
+        >
           <a href={id} className=" text-wrap">
             <i class="ri-medal-line"></i> {"  "}
             {designation}
