1c6af5ecdc48e48175233fcfd012bf20e8852957
Anirudh-yaduvanshi <yadavanirudh97.25@gmail.com>
2025-04-14 15:29:44 +0530

    notfound

--- src/Components/Pages/NotFound.jsx
+++ src/Components/Pages/NotFound.jsx
@@ -7,11 +7,11 @@ const NotFound = () => {
 
   return (
     <div className=" min-h-screen   ">
-      <div className="flex justify-center items-center flex-col gap-10 bg-red-950 h-screen text text-wrap text-white text-center">
+      <div className="flex justify-center items-center flex-col gap-10  h-screen text text-wrap text-white bg-gray-800 text-center">
         yo page ni hai kuvh bhi dhoond re ho! jo home pe dikh ra voi sufficient
         hai.... ku faltu me matha marra...
         <Link
-          className="text-white a bg-black rounded-lg hover:bg-blue-600 no-underline font-semibold px-4 py-2  hover:scale-95"
+          className="text-white bg-black rounded-lg hover:bg-blue-600 no-underline font-semibold px-6 py-3  hover:scale-95"
           to="/"
         >
           Home
